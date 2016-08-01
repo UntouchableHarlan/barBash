76823444952bdae3ab5c5f0f835b4501b1bb4739
Salomon <salomonpluviose@yahoo.com>
2016-07-31 23:42:04 -0400

    added more styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,63 +15,6 @@
 //= require turbolinks
 //= require_tree .
 
-// setInterval(function() {
-//    var day = new Date();
-//    var time = day.toLocaleTimeString();
-//    document.getElementById("hour").innerHTML = time;
-//  }, 1000);
-
-
-
-
-
-// var seconds = 60;
-// var type = "";
-// 		//
-//     var seconds= 10;
-//     var interval = setInterval(function() {
-//       var min = parseInt(seconds/60);
-//       sec = (seconds % 60)
-//       min = parseInt(sec /60)
-//       seconds -= 1
-//        document.getElementById("hour").innerHTML = min + ":" + sec;
-//        if (seconds === -1){
-// 				  seconds = 10;
-// 				$('.table').fadeOut(100).load("/ .reload").fadeIn(1000);
-// 				 setInterval(interval).fadeOut(100).fadeIn(1000)
-//
-//      }
-//      }, 1000);
-
-// function format(min, sec) {
-// 	if (sec === 0){
-// 		return min + ":" + "00"
-//
-// 	} else if(sec < 10){
-// 		return (min + ":0" + sec).fontcolor("red")
-// 	}
-// 	else {
-// 		return min + ":" + sec
-// 	}
-// }
-//
-// var interval = setInterval(function() {
-// 	var min = parseInt(seconds/60);
-// 	var sec = (seconds % 60);
-//   min = parseInt(sec /60)
-//   seconds -= 1
-//   $("#hour").html(format(min, sec));
-//   if (seconds === -1){
-//     seconds = 60;
-//     refreshDrinks();
-//     setInterval(interval).fadeOut(100).fadeIn(1000)
-//   }
-// }, 1000);
-//
-// function refreshDrinks() {
-//   $('.table').fadeOut(100).load("/?type=" + type + " .reload").fadeIn(1000);
-// }
-
 
 $(window).scroll(function() {
 	// console.log("some")
@@ -113,6 +56,7 @@ var interval = setInterval(function() {
     // setInterval(interval).fadeOut(100).fadeIn(1000)
   }
 }, 1000);
+
 // Refreshes the prices
 function refreshDrinks() {
 	$('.modal').remove();
