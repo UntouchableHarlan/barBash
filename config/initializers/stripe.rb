62622d49c99dd4f893f070fccb3cbb00d7ba03b9
Salomon <salomonpluviose@yahoo.com>
2016-08-01 10:25:36 -0400

    added stripe button inside of modal

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => "sk_test_9PWNaHvvBKEu3c9wkTx8gJyg",
-  :secret_key      => "pk_test_62HeHVyg9ixt0DZ2WE0UdVZ1"
+  :publishable_key => "pk_test_62HeHVyg9ixt0DZ2WE0UdVZ1",
+  :secret_key      => "sk_test_9PWNaHvvBKEu3c9wkTx8gJyg"
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   resources :drinks
   resources :owners
   resources :sessions
+  resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
