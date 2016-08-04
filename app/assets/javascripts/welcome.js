346142a2fb4a8e4a946fbef5a754f99318ee2ec3
Zach Roberts <zach.ker.roberts@gmail.com>
2016-08-04 19:41:09 -0400

    graphs are now working

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -12,8 +12,8 @@ function ready() {
 		$('.beer_container').css("display", "none");
 		$('.cocktail_container').css("display", "none");
 		$('.best_deal_container').css("display", "flex");
-
 	});
+
 	$('#shot a').on('click', function(event){
 		event.preventDefault();
 		// type = 'shot';
@@ -21,8 +21,8 @@ function ready() {
 		$('.beer_container').css("display", "none");
 		$('.cocktail_container').css("display", "none");
 		$('.best_deal_container').css("display", "none");
-
 	});
+
 	$('#beerss a').on('click', function(event){
 		event.preventDefault();
 		$('.shots_container').css("display", "none");
@@ -32,6 +32,7 @@ function ready() {
 
 		// type = 'beer';
 	});
+
 	$('#cocktail a').on('click', function(event){
 		event.preventDefault();
 		$('.shots_container').css("display", "none");
@@ -74,79 +75,71 @@ function ready() {
 			amount: parseFloat(price) * 100
 		});
 	});
+
 	$('body').on('click', '#submitButton', function(e){
 		// e.preventDefault();
-
 	});
 
 	$('#arrow').addClass('animated bounce');
 
 
 	$('.starttimer').on('click', function(event) {
-	  console.log("This is happneing?")
-	  	event.preventDefault();
-	  var seconds = 10;
-	  function format(min, sec) {
-	  	if (sec === 0){
-	  		return min + ":" + "00"
+		console.log("This is happneing?")
+		event.preventDefault();
+		var seconds = 10;
+		function format(min, sec) {
+			if (sec === 0){
+				return min + ":" + "00"
 
-	  	} else if(sec < 10){
-	  		return (min + ":0" + sec).fontcolor("red")
-	  	}
-	  	else {
-	  		return min + ":" + sec
-	  	}
-	  }
-	var interval = setInterval(function() {
-		var min = parseInt(seconds/60);
-		var sec = (seconds % 60);
+			} else if(sec < 10){
+				return (min + ":0" + sec).fontcolor("red")
+			}
+			else {
+				return min + ":" + sec
+			}
+		}
 
-	  seconds -= 1;
-	  $("#hour").html(format(min, sec));
-		$('#stoptimer a').on('click', function(event) {
-			event.preventDefault();
-			clearInterval(interval)
-			$("#hour").html('0:00')
-		});
-	  if (seconds === 5){
-			// clearInterval(interval)
-			$.ajax({
-	      url: '/updateprices',
-	      success: function(){
-	        console.log('success!');
-					$('#hour').fadeOut(5000);
-					$('.tables_container').fadeOut(5000);
-					$('.tables_container').load("/ .tables_container")
-					// sleep(5000)
-					$('.tables_container').fadeIn(1000);
-					$('#hour').fadeIn(300, function(){
-						seconds = 300;
-					});
+		var interval = setInterval(function() {
+			var min = parseInt(seconds/60);
+			var sec = (seconds % 60);
 
-
-
-					// setInterval(interval)
-
-	      }
+			seconds -= 1;
+			$("#hour").html(format(min, sec));
+			$('#stoptimer a').on('click', function(event) {
+				event.preventDefault();
+				clearInterval(interval)
+				$("#hour").html('0:00')
 			});
-
-
-	  }
-	}, 1000);
+			if (seconds === 5){
+				// clearInterval(interval)
+				$.ajax({
+					url: '/updateprices',
+					success: function(){
+						console.log('success!');
+						$('#hour').fadeOut(5000);
+						$('.tables_container').fadeOut(5000);
+						$('.tables_container').load("/ .tables_container")
+						// sleep(5000)
+						$('.tables_container').fadeIn(1000);
+						$('#hour').fadeIn(300, function(){
+							seconds = 300;
+						});
+						// setInterval(interval)
+					}
+				});
+			}
+		}, 1000);
 	});
-	function refreshDrinks() {
 
-	}
-	function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
+	$('#graph').on('show.bs.modal', function (event) {
+	  var button = $(event.relatedTarget);
+		var data = button.data('data');
+		var name = button.data('name');
+		new Chartkick.LineChart("chart", data, {});
+		$('.modal-title').text(name);
+	});
 }
 
-}
 $(document).on('ready turbolinks:load', ready);
 
 
